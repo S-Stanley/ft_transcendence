39787b36afd6a8070c0d0021eb9742fc97e0f072
Stanley SERBIN <stanleyserbin@gmail.com>
2022-11-11 13:08:36 +0100

    feat(database): create another user into the fixtures #19

--- database/fixtures/users-fixtures.sql
+++ database/fixtures/users-fixtures.sql
@@ -4,3 +4,10 @@ VALUES (
     'demo@42.fr',
     crypt('password', gen_salt('bf',11))
 );
+
+INSERT INTO
+    public.users (email, pass)
+VALUES (
+    'staff@transcendence.fr',
+    crypt('password', gen_salt('bf',11))
+);
