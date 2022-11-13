d1c798084265e4b189baa98bf196846773b01bcc
Jules Maisonneuve <julesm@hotmail.fr>
2022-11-13 19:28:26 +0100

    feature: auth42 in progress

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -6,13 +6,15 @@ import { ConfigModule } from '@nestjs/config';
 import { DbModule } from './config';
 import { LoggerMiddleware } from './middleware';
 import { ChatController } from './chat/chat.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
     imports: [ConfigModule.forRoot({
         isGlobal: true,
         envFilePath: ".env",
       }),
-      DbModule
+      DbModule,
+      HttpModule
     ],
     controllers: [AppController, UsersController, ChatController],
     providers: [AppService],
--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -1,48 +1,33 @@
 import { Controller, Post, Body, HttpException, Param, Get } from '@nestjs/common';
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable, Inject, Query } from '@nestjs/common';
+import { NestApplicationContextOptions } from '@nestjs/common/interfaces/nest-application-context-options.interface';
+import axios from 'axios';
+import { AxiosResponse } from 'axios';
+import { HttpService } from '@nestjs/axios';
+import { Observable } from 'rxjs';
 
+
+interface SSOReturn {
+    access_token: string;
+    expires_in: string;
+    refresh_token: string;
+    created_at: string;
+}
 @Controller('users')
 @Injectable()
-
 export class UsersController {
-    constructor(@Inject("PG_CONNECTION") private db: any){}
+    constructor(private readonly httpService: HttpService) {}
 
-    @Post('/auth/login')
-    Login(@Body() body): string {
-        return this.db.query("SELECT * FROM public.login($1, $2)", [body.email, body.password])
-            .then((result: { rows: any }) => {
-                if (result.rows.length === 0) {
-                    throw new HttpException('User does not exist', 500);
-                } else {
-                    return ({
-                        user_id: result.rows[0].id,
-                        email: result.rows[0].email,
-                        token: result.rows[0].token,
-                    });
-                }
-            })
-            .catch((e: any) => {
-                console.error(e);
-                throw new HttpException('Email or password incorrect', 500);
-            });
+    @Post('/auth')
+    auth(@Body() body: { code: string }): { token: string } {
+        console.log(this.getToken());
+        return
+        {
+            token: "kek"
+        };
     }
 
-    @Get('/:email')
-    findUserById(@Param() params): string {
-        return this.db.query("SELECT * FROM public.users where email=$1", [params?.email])
-            .then((result: { rows: any }) => {
-                if (result.rows.length === 0) {
-                    throw new HttpException('User does not exist', 500);
-                } else {
-                    return ({
-                        user_id: result.rows[0].id,
-                        email: result.rows[0].email,
-                    });
-                }
-            })
-            .catch((e: any) => {
-                console.error(e);
-                throw new HttpException('There was an error, please try again later', 500);
-            });
+    getToken(): Observable<AxiosResponse<SSOReturn>> {
+        return this.httpService.get('https://api.intra.42.fr/oauth/token');
     }
 }
