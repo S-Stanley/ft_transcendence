53b57a05eef9c9c6e05734416f9e4b4c1ba0e9cc
Jules Maisonneuve <julesm@hotmail.fr>
2022-11-14 17:01:47 +0100

    auth42 wip

--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -20,14 +20,51 @@ export class UsersController {
 
     @Post('/auth')
     auth(@Body() body: { code: string }): { token: string } {
-        console.log(this.getToken());
+        this.getToken(body.code).then((value) => {
+            console.log(value);
+        }, (error) => {
+            console.error(error);
+        });
         return
         {
             token: "kek"
         };
     }
 
-    getToken(): Observable<AxiosResponse<SSOReturn>> {
-        return this.httpService.get('https://api.intra.42.fr/oauth/token');
+    async getToken(code: string): Promise<AxiosResponse<SSOReturn>> {
+        const axios = require('axios');
+        const FormData = require('form-data');
+        var bodyFormData = new FormData();
+        bodyFormData.append('grant_type', 'authorization_code');
+        bodyFormData.append('client_id', 'u-s4t2ud-6694130f6db913ca3cd2ea1112756a01c8af82f732601f915369707b205c3b1c');
+        bodyFormData.append('client_secret', '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');
+        bodyFormData.append('code', code);
+        bodyFormData.append('redirect_uri', 'http://localhost:3000/oauth2-redirect');
+        console.log(bodyFormData)
+        const response = await this.httpService.post(
+            'https://api.intra.42.fr/oauth/token',
+            bodyFormData,
+            { headers: bodyFormData.getHeaders() }
+          ).toPromise();
+          console.log(response.data);
+          return response.data;
+        // var config = {
+        //     method: 'post',
+        //     url: 'https://api.intra.42.fr/oauth/token',
+        //     headers: { 
+        //       'Cookie': '_intra_42_session_production=119bd8710674aedd5136214a8f98ad9e; _mkra_stck=76c872e3efb1aee4e241b156a75a27f2%3A1668394038.2087028', 
+        //       ...bodyFormData.getHeaders()
+        //     },
+        //     data : bodyFormData
+        //   };
+          
+        //   const respons = axios(config)
+        // //   .then(function (response) {
+        // //     console.log(JSON.stringify(response.data));
+        // //   })
+        // //   .catch(function (error) {
+        // //     console.log(error);
+        // //   });
+        //   return respons.data;
     }
 }
