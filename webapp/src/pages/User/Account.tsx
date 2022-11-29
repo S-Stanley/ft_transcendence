c40ba5f54098e3a351eabd87771864b601e3ec05
automaths <nicolassartral@gmail.com>
2022-11-29 18:12:53 +0100

    fix(webapp): correct user page #72

--- webapp/src/pages/User/Account.tsx
+++ webapp/src/pages/User/Account.tsx
@@ -12,51 +12,7 @@ const Account = () => (
 			}}>
 			<AccountProfile />
 		</Box>
-
 			<AccountProfileDetails />
-
-
-
-			{/* <Box
-				component="main"
-				// sx={{
-				// 	flexGrow: 1,
-				// 	py: 8
-				// }}
-			>
-				<Container maxWidth="lg">
-					<Typography
-						sx={{ mb: 3 }}
-						variant="h4"
-					>
-						Mighty Pong Account
-					</Typography>
-					<Grid
-						container
-						// spacing={3}
-					>
-						<Grid
-							item
-							lg={4}
-							md={6}
-							xs={12}
-						>
-						<AccountProfile />
-						</Grid>
-					</Grid>
-				</Container>
-						<Grid
-							item
-							lg={8}
-							md={6}
-							xs={12}
-							sx={{
-								ml:'100px'
-							}}
-						>
-						<AccountProfileDetails />
-						</Grid>
-			</Box> */}
 		</>
 	);
 
